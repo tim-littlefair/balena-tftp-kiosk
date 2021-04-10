5dd20d86bad17f345f4d3fc5b077c97b6878f532
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2021-04-10 15:04:44 +0800

    X coming up and wireshark visible

--- src/autostart.sh
+++ src/autostart.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env sh
 
-syslogd
-/usr/sbin/in.tftpd -L -c -s -p -v -v -v /root/tftproot &
-lxterminal tail -f /var/log/syslog 
+touch /var/log/syslog
+syslogd &
+/usr/sbin/in.tftpd -L -c -s -p -vvv /root/tftproot &
+lxterminal tail -f /var/log/syslog & 
 wireshark -i eth0 &
 
 
