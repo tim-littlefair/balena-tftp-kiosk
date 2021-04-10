d718a0a60034802b1cd4f4479d133171e8840c9a
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2021-04-10 17:49:48 +0800

    WIP - wireshark starting up but not tail

--- src/autostart.sh
+++ src/autostart.sh
@@ -1,9 +1,6 @@
 #! /usr/bin/env sh
 
-touch /var/log/syslog
-syslogd &
-/usr/sbin/in.tftpd -L -c -s -p -vvv /root/tftproot &
-lxterminal tail -f /var/log/syslog & 
-wireshark -i eth0 &
+wireshark -i eth0 -i wlan0  -k &
+lxterminal -e tail -f /var/log/syslog & 
 
 
